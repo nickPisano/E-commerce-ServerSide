24965cb7a10e206e213c07bb79a1f1e7a0dc3a3c
Nick Pisano <pisano.nicholas@yahoo.com>
2022-08-16 23:49:28 -0700

    Changed Product Routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -9,9 +9,13 @@ router.get('/', async  (req, res) => {
   // be sure to include its associated Category and Tag data
   try {
     const productData = await Product.findAll({
-      include: [{
-        model: Category, model: Tag
-      }]
+      include: [
+        Category,
+        {
+          model: Tag,
+          through: ProductTag
+        }
+      ]
     });
     res.status(200).json(productData);
   } catch (err) {
